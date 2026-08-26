0287e15a7d18994ff45dcc34e176cd2c0a343368
hbngoc216 <baongochuynh113@gmail.com>
2026-08-26 17:33:32 +0000

    Test-final

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Node.js CI/CD Demo!');
+  res.send('Hello World from Node.js CI/CD Demo!-27-8-2026');
 });
 
 app.get('/health', (req,res) => {
